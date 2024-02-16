a6a2b4f3fdbb816a90ee0b45c11d21868ebba80f
Ziyang Qin <zq38@cornell.edu>
2024-02-16 11:16:28 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@ from pgmpy.models import DynamicBayesianNetwork as DBN
 from DBN_toolbox import qzy
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -27,15 +28,18 @@ dbn.add_edges_from([
 # plt.ylabel('Frequency')
 # plt.show()
 
-# T0
-qzy.DBN_train(dbn, bins = [4, 3, 3])
-qzy.DBN_evaluate(dbn)
+# # T0
+# qzy.DBN_train(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 3, 3], shuffled=True, seed=1021)
+# qzy.DBN_evaluate(dbn)
 
 # # T1
 # i = 1001
 # accs = []
-# n = 1499
-# while i <= n:
+# number_of_iterations = 20
+# maxn = 1000+number_of_iterations
+# highest_accuracy = -999
+# highest_seed = -999
+# while i <= maxn:
 #     dbn.clear()
 #     dbn = DBN()
 #     dbn.add_edges_from([
@@ -48,12 +52,18 @@ qzy.DBN_evaluate(dbn)
 #         # participant 1 removed
 #     ])
 
-#     qzy.DBN_train(dbn, "Agent_UpdatedTra_Simulation.csv", True, i)
-#     print(str(i-1000) + "/" + str(n-1000))
-#     accs.append(qzy.DBN_acc(dbn))
+#     qzy.DBN_train(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 3, 3], shuffled=True, seed=i)
+#     print(str(i-1000) + "/" + str(number_of_iterations))
+#     accuracy = qzy.DBN_acc(dbn)
+#     if (accuracy > highest_accuracy):
+#         highest_accuracy = accuracy
+#         highest_seed = i
+#     accs.append(accuracy)
 #     i += 1
 
-# qzy.plot_normal_distribution(accs)
+# print(f"highest accuracy: {highest_accuracy}")
+# print(f"seed with highest accuracy: {highest_seed}")
+# # qzy.plot_normal_distribution(accs)
 
 
 # T2
@@ -61,3 +71,27 @@ qzy.DBN_evaluate(dbn)
 
 # # Hyperdata
 # qzy.DBN_hyper(dbn, ran = 11)
+
+# # T3
+# qzy.DBN_T3(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 3, 3], shuffled=True, seed=1021)
+# qzy.DBN_evaluate(dbn)
+
+# # T4
+# qzy.DBN_T4(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 5, 3])
+# qzy.DBN_evaluate(dbn)
+
+# # Hyperdata T4
+# qzy.DBN_T4_hyper(dbn, ran = 11)
+
+# # Logistic Regression
+# print(qzy.Logit(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 5, 3], shuffled=True, seed=99))
+
+# # Random_Forest
+# print(qzy.RandomForestModel(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 5, 3], shuffled=True, seed=124))
+
+# # Test qzy.DBN_acc_and_sensitivity
+# qzy.DBN_train(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 3, 3], shuffled=True, seed=1021)
+# print(qzy.DBN_acc_and_sensitivity(dbn))
+
+# Compare models
+qzy.compare_models()
