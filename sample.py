7a498d21534f2110c934de2e55b6f181cc7c1c63
Ziyang Qin <zq38@cornell.edu>
2024-03-21 21:23:55 -0400

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,15 +8,15 @@ print(f"Core number: {cores}")
 # predictors = ['Occlusivity', 'Visible_Helpful_Sign', 'Time_Helpful_SignSeen', 'Num_intersection', 'group', 'sbsod', 'sa', 'mt', 'age']
 # local_bins = [3, 5, 5, 3, 4, 3, 4, 5, 3, 3]
 
-variables = ['Occlusivity', 'Time_Helpful_SignSeen', 'group', 'mt', 'age', 'uncertain']
-predictors = ['Occlusivity', 'Time_Helpful_SignSeen', 'group', 'mt', 'age']
-local_bins = [3, 5, 4, 5, 3, 3]
+variables = ['Num_intersection', 'Time_Helpful_SignSeen', 'sbsod', 'framecount', 'uncertain']
+predictors = ['Num_intersection', 'Time_Helpful_SignSeen', 'sbsod', 'framecount']
+local_bins = [5, 4, 4, 3, 3] 
 
 network = qzy.DBN_ini(predictors)
 
 # # T0
-test_data, network = qzy.DBN_train(network, columns=variables, data_name="Agent_UpdatedTra_Simulation.csv", bins = local_bins, shuffled=True, seed=3524, fast=True)
-qzy.DBN_evaluate(network=network, test_data=test_data, variables_to_add=predictors)
+# test_data, network = qzy.DBN_train(network, columns=variables, data_name="Agent_UpdatedTra_Simulation.csv", bins = local_bins, shuffled=True, seed=3524, fast=True)
+# qzy.DBN_evaluate(network=network, test_data=test_data, variables_to_add=predictors)
 
 # # fast
 # test_data, network = qzy.DBN_train(network, columns=variables, data_name="Agent_UpdatedTra_Simulation.csv", bins = local_bins, shuffled=True, seed=1097, fast=True)
@@ -25,14 +25,14 @@ qzy.DBN_evaluate(network=network, test_data=test_data, variables_to_add=predicto
 # # T1
 # qzy.DBN_T1(network, predictors=predictors, number_of_iterations=8, variables=variables, bins=local_bins)
 
-# # T1_multithreaded
-# qzy.DBN_T1_multithreaded(network, number_of_iterations=100, num_threads=cores, predictors=predictors, variables=variables, bins=local_bins)
+# T1_multithreaded
+qzy.DBN_T1_multithreaded(network, number_of_iterations=500, num_threads=cores, predictors=predictors, variables=variables, bins=local_bins)
 
 # T2
 # qzy.DBN_T2(network, "Agent_UpdatedTra_Simulation.csv", 123)
 
 # # Hyperdata
-# qzy.DBN_hyper(network, columns=variables, predictors=predictors, ran = 5, seed=3524)
+# qzy.DBN_hyper(network, columns=variables, predictors=predictors, ran = 8, seed=1019)
 
 # # T3
 # qzy.DBN_T3(network=dbn, data_name="Agent_UpdatedTra_Simulation.csv",bins = [4, 3, 3], shuffled=True, seed=1021)
